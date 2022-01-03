a850cb24e31b573d88ef20675cf3cd755c25c167
woosung <jungyh0528@gmail.com>
2022-01-04 01:58:23 +0900

    MainPage

--- frontend/src/page/main/index.js
+++ frontend/src/page/main/index.js
@@ -1,18 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import Styled from 'styled-components';
-import { Box, TextField, Button, Typography } from '@mui/material'
+import { Box, TextField, Button, Typography } from '@mui/material';
 
 const Wrapper = Styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 95vw;
+  height: 95vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: white;
+  background-color: black;
 `;
 
 function MainPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return(
     <Wrapper>
       <Box sx={{
@@ -20,15 +23,17 @@ function MainPage() {
         height: 400,
         display: 'flex',
         flexDirection: 'column',
-        alignItems : 'center',
-        justifyContent : 'center',
-        backgroundColor : 'white',
-        border : '1px solid black'
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderRadius: 3,
+        padding: '8px',
       }}>
-        <Typography>Login</Typography>
-        <TextField label="Email" />
-        <TextField label="PassWord"/>
-        <Button variant="contained">Submit</Button>
+        <Typography sx={{ margin : '8px', marginBottom : '32px'}} variant='h4'>Login</Typography>
+        <TextField sx={{ width : 300, margin : '8px'}} label="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <TextField sx={{ width : 300, margin : '8px'}} label="PassWord" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <Button sx={{ margin : '8px'}} variant="contained" onClick={() => console.log(email, password)}>Submit</Button>
       </Box>
     </Wrapper>
   )
